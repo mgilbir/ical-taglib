f094de212ef19842538a4fc2f85840052ba309e8
jjadonotenter <jjadonotenter@2f31463c-7d2d-0410-9a13-9362cc368a46>
2010-11-05 17:15:58 +0000

    - trim content

--- 0.1/src/net/franchu/taglib/ical/DateEndTag.java
+++ 0.1/src/net/franchu/taglib/ical/DateEndTag.java
@@ -17,65 +17,57 @@ import net.fortuna.ical4j.model.property.DtEnd;
 
 /**
  * @author mgilbir
- *
+ * 
  */
 public class DateEndTag extends EventBodyTag {
-	
+
 	private String format = "yyyy-MM-dd'T'HH:mm:ss";
 	private String timezone = null;
 	private Value value = Value.DATE_TIME;
-	
-	public void setFormat(String format)
-    {
-        this.format=format;
-    }
-	
-	public void setTimezone(String timezone)
-    {
-        this.timezone=timezone;
-    }
-	
-	public void setValue(String value) throws JspTagException
-	{
-		if(value.toLowerCase().equals("date")) 
-		{
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
+	}
+
+	public void setValue(String value) throws JspTagException {
+		if (value.toLowerCase().equals("date")) {
 			this.value = Value.DATE;
-		}else if(value.toLowerCase().equals("date-time"))
-		{
+		} else if (value.toLowerCase().equals("date-time")) {
 			this.value = Value.DATE_TIME;
-		}else
-		{
-			throw new JspTagException("DateStartTag invalid Valuetype attribute: " + value);
+		} else {
+			throw new JspTagException(
+					"DateStartTag invalid Valuetype attribute: " + value);
 		}
 	}
 
-
-	void ProcessBody(String body) throws JspTagException
-	{
+	void ProcessBody(String body) throws JspTagException {
+		body = body.trim();
 		SimpleDateFormat sdf = new SimpleDateFormat(format);
 		DtEnd end = new DtEnd();
 		Calendar calendar = Calendar.getInstance();
-		try 
-		{
+		try {
 			java.util.Date date = sdf.parse(body);
-			if (timezone != null)
-			{
+			if (timezone != null) {
 				calendar.setTime(date);
 				calendar.setTimeZone(java.util.TimeZone.getTimeZone(timezone));
 				date = calendar.getTime();
 			}
-			end.setDate(this.value.equals(Value.DATE)?new Date(date):new DateTime(date));
-			
-		} 
-		catch (Exception e) 
-		{
-			throw new JspTagException("DateEndTag invalid string format: " + e.getMessage());
+			end.setDate(this.value.equals(Value.DATE) ? new Date(date)
+					: new DateTime(date));
+
+		} catch (Exception e) {
+			throw new JspTagException("DateEndTag invalid string format: "
+					+ e.getMessage());
 		}
-     		
+
 		VEvent event = parent.event;
 		event.getProperties().add(end);
-		event.getProperties().getProperty(Property.DTEND).getParameters().add(this.value);
-		//throw new JspTagException("Testing: " + end.getValue());
+		event.getProperties().getProperty(Property.DTEND).getParameters().add(
+				this.value);
+		// throw new JspTagException("Testing: " + end.getValue());
 	}
-
 }
