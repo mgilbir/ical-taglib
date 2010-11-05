dc92e04608646c7a92217872a54397a48416f620
jjadonotenter <jjadonotenter@2f31463c-7d2d-0410-9a13-9362cc368a46>
2010-11-05 17:17:00 +0000

    - cleanup

--- trunk/0.1/src/net/franchu/taglib/ical/EventBodyTag.java
+++ trunk/0.1/src/net/franchu/taglib/ical/EventBodyTag.java
@@ -9,48 +9,45 @@ import javax.servlet.jsp.tagext.BodyTagSupport;
 
 /**
  * @author mgilbir
- *
+ * 
  */
 public class EventBodyTag extends BodyTagSupport {
 
 	EventTag parent;
-	
-	public int doStartTag() throws JspTagException 
-	{
-	   parent =  (EventTag)findAncestorWithClass(this, EventTag.class);
-	   if (parent == null)
-	   {
-		   throw new JspTagException("nesting error");
-	   }
-	   
-	   /* Avoids several BodyTagSupport tags to be nested */ 
-	   /*BodyTagSupport temp = (BodyTagSupport)findAncestorWithClass(this, BodyTagSupport.class);
-	   if(temp != null)
-	   {
-		   throw new JspTagException("nesting error");		   
-	   }*/
-	   return(EVAL_BODY_TAG);
+
+	public int doStartTag() throws JspTagException {
+		parent = (EventTag) findAncestorWithClass(this, EventTag.class);
+		if (parent == null) {
+			throw new JspTagException("nesting error");
+		}
+
+		/* Avoids several BodyTagSupport tags to be nested */
+		/*
+		 * BodyTagSupport temp = (BodyTagSupport)findAncestorWithClass(this,
+		 * BodyTagSupport.class); if(temp != null) { throw new
+		 * JspTagException("nesting error"); }
+		 */
+		return (EVAL_BODY_TAG);
 	}
-	
+
 	public int doAfterBody() throws JspTagException {
-	      BodyContent bc = getBodyContent();
-	      String body;
-	      try 
-	      {
-	      //Do the data handling
-	    	  body = bc.getString();
-	    	  bc.clearBody();
-	    	  ProcessBody(body);
-	    	  
-	      } catch (Exception e) {
-	         throw new JspTagException("Tag " + this.getClass().getName() +": " + e.getMessage());
-	      }
-	      return SKIP_BODY;
-	   }
-	
-	void ProcessBody(String body) throws JspTagException
-	{
-	    throw new JspTagException("Method Overriding not working");	
+		BodyContent bc = getBodyContent();
+		String body;
+		try {
+			// Do the data handling
+			body = bc.getString();
+			bc.clearBody();
+			ProcessBody(body);
+
+		} catch (Exception e) {
+			throw new JspTagException("Tag " + this.getClass().getName() + ": "
+					+ e.getMessage());
+		}
+		return SKIP_BODY;
 	}
-	
+
+	void ProcessBody(String body) throws JspTagException {
+		throw new JspTagException("Method Overriding not working");
+	}
+
 }
--- trunk/0.1/src/net/franchu/taglib/ical/RepeatTag.java
+++ trunk/0.1/src/net/franchu/taglib/ical/RepeatTag.java
@@ -3,15 +3,13 @@
  */
 package net.franchu.taglib.ical;
 
-
 /**
  * @author mgilbir
- *
+ * 
  */
 public class RepeatTag extends EventBodyTag {
 
-	void ProcessBody(String body)
-	{
-		
+	void ProcessBody(String body) {
+
 	}
 }
