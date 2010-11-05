3869740eedda1480e24653337d8be9d5f54505c7
jjadonotenter <jjadonotenter@2f31463c-7d2d-0410-9a13-9362cc368a46>
2010-11-05 17:17:00 +0000

    - cleanup

--- 0.1/src/net/franchu/taglib/ical/EventTag.java
+++ 0.1/src/net/franchu/taglib/ical/EventTag.java
@@ -7,31 +7,28 @@ import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 
 import net.fortuna.ical4j.model.component.VEvent;
+
 /**
  * @author mgilbir
- *
+ * 
  */
-public class EventTag extends BodyTagSupport
-{
+public class EventTag extends BodyTagSupport {
 	CalendarTag parent;
 	VEvent event;
-	
-	public int doStartTag() throws JspTagException 
-	{
-	   parent =  (CalendarTag)findAncestorWithClass(this, CalendarTag.class);
-	   if (parent == null)
-	   {
-		   throw new JspTagException("nesting error");
-	   }
-	   
-	   event = new VEvent();
-	   return(EVAL_BODY_TAG);
+
+	public int doStartTag() throws JspTagException {
+		parent = (CalendarTag) findAncestorWithClass(this, CalendarTag.class);
+		if (parent == null) {
+			throw new JspTagException("nesting error");
+		}
+
+		event = new VEvent();
+		return (EVAL_BODY_TAG);
 	}
-	
-	public int doEndTag() throws JspTagException 
-	{
-	      parent.calendar.getComponents().add(event);
-	      return SKIP_BODY;
+
+	public int doEndTag() throws JspTagException {
+		parent.calendar.getComponents().add(event);
+		return SKIP_BODY;
 	}
 
 }
